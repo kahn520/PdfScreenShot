c8d2e3620d687f910dec85185e3d10df32bfb34b
misear <zhong.ch22@gmail.com>
2017-03-23 21:57:42 +0800

    111

--- PdfScreenShot/Form1.Designer.cs
+++ PdfScreenShot/Form1.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.radFile = new System.Windows.Forms.RadioButton();
+            this.radFolder = new System.Windows.Forms.RadioButton();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnPath = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.listMsg = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // radFile
+            // 
+            this.radFile.AutoSize = true;
+            this.radFile.Checked = true;
+            this.radFile.Location = new System.Drawing.Point(27, 25);
+            this.radFile.Name = "radFile";
+            this.radFile.Size = new System.Drawing.Size(69, 22);
+            this.radFile.TabIndex = 0;
+            this.radFile.TabStop = true;
+            this.radFile.Text = "文件";
+            this.radFile.UseVisualStyleBackColor = true;
+            // 
+            // radFolder
+            // 
+            this.radFolder.AutoSize = true;
+            this.radFolder.Location = new System.Drawing.Point(139, 25);
+            this.radFolder.Name = "radFolder";
+            this.radFolder.Size = new System.Drawing.Size(87, 22);
+            this.radFolder.TabIndex = 0;
+            this.radFolder.Text = "文件夹";
+            this.radFolder.UseVisualStyleBackColor = true;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 64);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(380, 28);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnPath
+            // 
+            this.btnPath.Location = new System.Drawing.Point(398, 58);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(75, 37);
+            this.btnPath.TabIndex = 2;
+            this.btnPath.Text = "选择";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(12, 114);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(116, 39);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "开始";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // listMsg
+            // 
+            this.listMsg.FormattingEnabled = true;
+            this.listMsg.ItemHeight = 18;
+            this.listMsg.Location = new System.Drawing.Point(12, 170);
+            this.listMsg.Name = "listMsg";
+            this.listMsg.Size = new System.Drawing.Size(476, 310);
+            this.listMsg.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(500, 492);
+            this.Controls.Add(this.listMsg);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnPath);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.radFolder);
+            this.Controls.Add(this.radFile);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pdf截图";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton radFile;
+        private System.Windows.Forms.RadioButton radFolder;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.ListBox listMsg;
     }
 }
 
--- PdfScreenShot/Form1.cs
+++ PdfScreenShot/Form1.cs
@@ -3,17 +3,166 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
+using Acrobat;
 
 namespace PdfScreenShot
 {
     public partial class Form1 : Form
     {
+        private Thread thread = null;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void btnPath_Click(object sender, EventArgs e)
+        {
+            if (radFile.Checked)
+            {
+                OpenFileDialog fd = new OpenFileDialog();
+                fd.FileName = "";
+                fd.Filter = "PDF|*.pdf";
+                if (fd.ShowDialog() == DialogResult.OK)
+                {
+                    txtPath.Text = string.Join(";", fd.FileNames);
+                }
+            }
+            else
+            {
+                FolderBrowserDialog fd = new FolderBrowserDialog();
+                if (fd.ShowDialog() == DialogResult.OK)
+                {
+                    txtPath.Text = fd.SelectedPath;
+                }
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (thread == null || !thread.IsAlive)
+            {
+                thread = new Thread(DoWork);
+                thread.SetApartmentState(ApartmentState.STA);
+                thread.Start();
+            }
+        }
+
+        private void DoWork()
+        {
+            string[] strsFiles = GetFiles();
+            if (strsFiles.Length > 0)
+            {
+                int i = 1;
+                strsFiles.ToList().ForEach(f =>
+                {
+                    listMsg.Items.Clear();
+                    listMsg.Items.Add($"正在处理第{i}个文件(共{strsFiles.Length}个文件)");
+                    string strExportPath = Path.GetDirectoryName(f) + "\\" + Path.GetFileNameWithoutExtension(f);
+                    ExportAsImage(f, strExportPath);
+                    i++;
+                });
+                listMsg.Items.Add("完成");
+                listMsg.TopIndex = listMsg.Items.Count - 1;
+                MessageBox.Show("完成");
+            }
+        }
+
+        private void ExportAsImage(string strFile, string strExportPath)
+        {
+            if (!Directory.Exists(strExportPath))
+            {
+                Directory.CreateDirectory(strExportPath);
+            }
+            Acrobat.AcroPDDoc pdf = new AcroPDDoc();
+            pdf.Open(strFile);
+
+            int count = pdf.GetNumPages();
+            for (int i = 0; i < count; i++)
+            {
+                listMsg.Items.Add($"正在导出第{i + 1}页(共{count}页)");
+                listMsg.TopIndex = listMsg.Items.Count - 1;
+                int zoom = 1;
+                CAcroPDPage page = (CAcroPDPage)pdf.AcquirePage(i);
+                CAcroRect pdfRect = (Acrobat.CAcroRect)Microsoft.VisualBasic.Interaction.CreateObject("AcroExch.Rect", "");
+                CAcroPoint pdfPoint = (Acrobat.CAcroPoint)page.GetSize();
+                int imgWidth = (int)((double)pdfPoint.x * zoom);
+                int imgHeight = (int)((double)pdfPoint.y * zoom);
+
+                //设置裁剪矩形的大小为当前页的大小  
+                pdfRect.Left = 0;
+                pdfRect.right = (short)imgWidth;
+                pdfRect.Top = 0;
+                pdfRect.bottom = (short)imgHeight;
+                page.CopyToClipboard(pdfRect, 0, 0, (short)(100 * zoom));
+                Object obj = Clipboard.GetData(DataFormats.Bitmap);
+                using (Bitmap bitmap = (Bitmap)obj)
+                {
+                    SavePicture(bitmap, strExportPath, Path.GetFileNameWithoutExtension(strFile), (i + 1).ToString());
+                }
+            }
+        }
+
+        private void SavePicture(Bitmap bitmap, string strExportPath, string strFileName, string strPage)
+        {
+            Size[] size;
+            if (bitmap.Width < bitmap.Height)
+            {
+                size = new Size[] {new Size(721, 1020), new Size(162, 229), new Size(120, 174)};
+            }
+            else
+            {
+                size = new Size[] { new Size(721, 540), new Size(210, 117), new Size(120, 89) };
+            }
+            string strBig = $@"{strExportPath}\{strFileName}_{strPage}.jpg";
+            using (Bitmap bt = new Bitmap(bitmap, size[0]))
+            {
+                bt.Save(strBig, ImageFormat.Jpeg);
+            }
+            if (strPage == "1")
+            {
+                string strMidele = $@"{strExportPath}\m_{strFileName}_1.jpg";
+                using (Bitmap bt = new Bitmap(bitmap, size[1]))
+                {
+                    bt.Save(strMidele, ImageFormat.Jpeg);
+                }
+            }
+            string strSmall = $@"{strExportPath}\1_{strFileName}_{strPage}.jpg";
+            using (Bitmap bt = new Bitmap(bitmap, size[2]))
+            {
+                bt.Save(strSmall, ImageFormat.Jpeg);
+            }
+        }
+
+        private string[] GetFiles()
+        {
+            if (txtPath.Text.Contains(";"))
+            {
+                return txtPath.Text.Split(';');
+            }
+            else
+            {
+                if (Directory.Exists(txtPath.Text))
+                {
+                    return Directory.GetFiles(txtPath.Text, "*.pdf");
+                }
+                else if (File.Exists(txtPath.Text))
+                {
+                    return new string[1] {txtPath.Text};
+                }
+            }
+            return new string[0];
         }
     }
 }
